b67d2027766e6a15f97aea3dae3d3853bad2a7b0
ladybug908 <a.ladybug.21@gmail.com>
2020-09-06 17:19:24 +0900

    リダイレクト処理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
+  before_action :move_to_new
 
   def new
   end
 
   def destroy
+  end
+
+  private
+
+  def move_to_new
+    unless user_signed_in?
+      redirect_to action: :new
+    end
   end
 end
