ffb40475baf5afa5778c53b8240a1f9be247fd13
ladybug908 <a.ladybug.21@gmail.com>
2020-09-06 17:10:00 +0900

    ユーザーテーブルのカラム追加

--- db/migrate/20200903090534_devise_create_users.rb
+++ db/migrate/20200903090534_devise_create_users.rb
@@ -4,8 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :frist_name,         null: false
+      t.string :family_name,        null: false
+      t.string :frist_name_kana,    null: false
+      t.string :family_name_kana,   null: false
+      t.date   :birth,              null: false
+
 
       ## Recoverable
       t.string   :reset_password_token
--- db/migrate/20200905001022_add_first_name_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddFirstNameToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :first_name, :string
-    add_column :users, :family_name, :string
-    add_column :users, :first_name_kana, :string
-    add_column :users, :family_name_kana, :string
-    add_column :users, :brith, :date
-  end
-end
