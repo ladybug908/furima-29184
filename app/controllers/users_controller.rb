f814e8ebfcf4a74c033b3837829fffd20bf6884d
ladybug908 <a.ladybug.21@gmail.com>
2020-09-07 11:47:50 +0900

    hoge編集＆バリデーション記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,23 @@ class UsersController < ApplicationController
   before_action :move_to_new
 
   def new
+    @user = User.new
   end
 
   def destroy
   end
 
+  def create
+    @user = User.create(user_params)
+  end
+
   private
+
+  def user_params
+    params.require(:user).permit(
+      :nickname, :frist_name, :family_name, :frist_name_kana, :family_name_kana, :birth
+    )
+  end
 
   def move_to_new
     unless user_signed_in?
