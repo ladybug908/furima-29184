2f6ee10608fda0b9a70d917a9a156a47e1a896bc
ladybug908 <a.ladybug.21@gmail.com>
2020-09-07 17:18:52 +0900

    FactoryBot,Faker　install

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    it "nicknameが空だと登録できない" do
+      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors.full_messages).to include("ニックネームを入力してください")
+    end
+    it "emailが空だと登録できない" do
+    end
+  end
+end
