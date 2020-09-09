62c6dc7a119edda182c5849984beac08568b17c9
ladybug908 <a.ladybug.21@gmail.com>
2020-09-09 11:36:10 +0900

    コントローラー　create,params記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,16 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :introduction, :category_id, :status_id, :postage_id, :area_id, :days_id, :price)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,18 @@
 ActiveRecord::Schema.define(version: 2020_09_04_032827) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "item_name", null: false
+    t.text "introduction", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "postage_id", null: false
+    t.integer "area_id", null: false
+    t.integer "days_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -36,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_09_04_032827) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
