7ab8fc9e6cc2d2297c393e906307ced1721ebdaf
ladybug908 <a.ladybug.21@gmail.com>
2020-09-09 15:22:39 +0900

    ログイン・未ログイン出品ページ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_new_user_session , only: :new
+
   def index
     @items = Item.all
   end
@@ -15,5 +17,11 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:item_name, :introduction, :category_id, :status_id, :postage_id, :area_id, :days_id, :price)
+  end
+
+  def move_to_new_user_session
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
