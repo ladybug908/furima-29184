47b15b28ea49ff26b15e9e8b28bb5d71773e873b
ladybug908 <a.ladybug.21@gmail.com>
2020-09-12 12:43:58 +0900

    テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name        { "サンプル" }
+    introduction     { "サンプル" }
+    category_id      { 1 }
+    status_id        { 1 }
+    postage_id       { 1 }
+    area_id          { 1 }
+    day_id           { 1 }
+    price            {10000}
+    association :user 
   end
 end
