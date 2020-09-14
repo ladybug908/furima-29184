65e95f07c7cd805c989c30f30a74bbcb94721e76
ladybug908 <a.ladybug.21@gmail.com>
2020-09-14 12:11:05 +0900

    レビューの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,7 @@ class Item < ApplicationRecord
 
   validates :image, presence: { message: "をアップロードしてください" }
 
-  validates :item_name, :introduction, :price, presence: true
-
-  
+  validates :item_name, :introduction, presence: true
 
   validates :item_name, length: { maximum: 40 }
   validates :introduction, length: { maximum: 1000 }
@@ -13,8 +11,10 @@ class Item < ApplicationRecord
   
   validates :category_id, :status_id, :postage_id, :area_id, :day_id, numericality: { other_than: 0, message: "を選択して下さい"}
 
-  validates :price, format: { with: /\A[0-9]+\z/ }
-  validates :price, numericality:{ greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  with_options presence: true do
+    validates :price, format: { with: /\A[0-9]+\z/ }
+    validates :price, numericality:{ greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  end
 
 
   belongs_to :user
