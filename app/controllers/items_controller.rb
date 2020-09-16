a1686d46dc59fbe4a584fa0b74ce79aaace3712d
ladybug908 <a.ladybug.21@gmail.com>
2020-09-16 20:58:55 +0900

    コントローラーの設定とエラーメッセージ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,12 +23,16 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @items = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
