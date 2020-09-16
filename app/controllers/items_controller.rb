0905644dbbad3dbe802e2499b511d21f62d96a8b
ladybug908 <a.ladybug.21@gmail.com>
2020-09-16 12:17:54 +0900

    rubocop実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :move_to_new_user_session , only: :new
+  before_action :move_to_new_user_session, only: :new
 
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
@@ -22,15 +22,13 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-
   private
+
   def item_params
-    params.require(:item).permit(:image, :item_name, :introduction, :category_id, :status_id, :postage_id, :area_id, :day_id, :price).merge(user_id: current_user.id,)
+    params.require(:item).permit(:image, :item_name, :introduction, :category_id, :status_id, :postage_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
   end
 
   def move_to_new_user_session
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,19 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  validates :image, presence: { message: "をアップロードしてください" }
+  validates :image, presence: { message: 'をアップロードしてください' }
 
   validates :item_name, :introduction, presence: true
 
   validates :item_name, length: { maximum: 40 }
   validates :introduction, length: { maximum: 1000 }
 
-  
-  validates :category_id, :status_id, :postage_id, :area_id, :day_id, numericality: { other_than: 0, message: "を選択して下さい"}
+  validates :category_id, :status_id, :postage_id, :area_id, :day_id, numericality: { other_than: 0, message: 'を選択して下さい' }
 
   with_options presence: true do
     validates :price, format: { with: /\A[0-9]+\z/ }
-    validates :price, numericality:{ greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
   end
-
 
   belongs_to :user
   belongs_to_active_hash :category
@@ -24,5 +22,4 @@ class Item < ApplicationRecord
   belongs_to_active_hash :area
   belongs_to_active_hash :day
   has_one_attached :image
-
 end
