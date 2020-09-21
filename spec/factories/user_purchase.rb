acaee35e2f6d459b2092f4eb2af214e2861b15fb
ladybug908 <a.ladybug.21@gmail.com>
2020-09-21 14:30:50 +0900

    テストコードの記述

---
+++ spec/factories/user_purchase.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user_purchase do
+    postal_code   {'123-4567'}
+    area_id       {1}
+    city          {'横浜市'}
+    block         {'青山1−１'}
+    build         {}
+    tel           {'09012345678'}
+
+    token {"sample"}
+    association :user
+    association :item
+  end
+end
