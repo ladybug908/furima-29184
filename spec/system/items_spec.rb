e92e0a35b4c46ff67f9eb504994219b5b24488e1
ladybug908 <a.ladybug.21@gmail.com>
2020-09-25 18:44:14 +0900

    サポートモジュール作成

---
+++ spec/system/items_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'ツイート投稿', type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+    @tweet_text = Faker::Lorem.sentence
+    @tweet_image = Faker::Lorem.sentence
+  end
+end
