9d3757a1b590ca85dd03d4fac299a54725b936a9
ladybug908 <a.ladybug.21@gmail.com>
2020-09-25 18:05:50 +0900

    ユーザー登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :basic_auth, if: :production?
+  
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :frist_name, :family_name, :frist_name_kana, :family_name_kana, :birth])
+  end
+
+  def production?
+    Rails.env.production?
   end
 
   def basic_auth
